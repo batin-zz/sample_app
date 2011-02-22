8c794c0fd924c8f991f01ddd456bd632d92dae0e
batin <batin.oleg@gmail.com>
2011-02-21 22:00:25 -0600

    Fixed Chapter 3 exercise 2

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,24 +2,23 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
-
+  
   before(:each) do
-    #
-    # Define @base_title here.
-    #
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
-
+   
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => "#{@base_title} | Home" )
+    end
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -28,12 +27,9 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => "#{@base_title} | Contact" )
     end
   end
 
@@ -42,13 +38,21 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => "#{@base_title} | About" )
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "#{@base_title} | Help")
+    end
+  end
+  
 end
-
