49c79b5f3e361d597a57303cc8322bd4983ab4fa
batin <batin.oleg@gmail.com>
2011-02-21 21:28:41 -0600

    Finished first cut of the User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+    def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @title = "Sign up"
+  end
 end
