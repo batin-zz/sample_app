b9baaae9fc86a5ce0c39cb8ab398162c6ca98880
batin <batin.oleg@gmail.com>
2011-02-28 15:19:14 -0600

    User model with passwords, and user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110222022510) do
+ActiveRecord::Schema.define(:version => 20110228194004) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
